5d9116dd44fd2d2ebcead78d7db391a05f70ea73
kovacsat2000 <kovacsat2000@gmail.com>
2020-05-27 14:25:58 +0200

    Checkstyle hozzáadva. Egyéb apróbb hibák javítva.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,6 @@
 module org.project {
     requires javafx.controls;
     requires org.tinylog.api;
-    requires org.apache.commons.lang3;
     requires com.google.gson;
 
     exports game.view to javafx.graphics;
